3ad47814c93ef80fd7c078ee3b28d4c3cb4841bf
倉持祐介 <user@DESKTOP-0QETH86.localdomain>
2022-09-30 16:51:33 +0900

    rspec実装

--- db/migrate/20220929050936_create_task_managers.rb
+++ db/migrate/20220929050936_create_task_managers.rb
@@ -1,4 +1,6 @@
 class CreateTaskManagers < ActiveRecord::Migration[6.1]
+  change_column :task_managers, :task, :detail, null: false
+
   def change
     create_table :task_managers do |t|
       t.string :task
